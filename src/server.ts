b3a8617fb57149af9d508f3dcf063c2d3bafedec
Igor Zakharov1 <igor_zakharov1@epam.com>
2021-10-25 14:50:12 +0300

    home-task-2 - basic server setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+import express from 'express';
+import helmet from 'helmet';
+
+import apiRouter from './api/router';
+
+const PUBLIC_PORT = Number(process.env.PORT || 3000);
+
+const server = express();
+server.use(helmet({ contentSecurityPolicy: false, referrerPolicy: false }));
+server.use(express.json({ limit: '300kb' }));
+server.use(express.urlencoded({ limit: '300kb', extended: true }));
+server.use(apiRouter);
+
+server.listen(PUBLIC_PORT, (): void => {
+  console.log(`> Ready on http://localhost:${PUBLIC_PORT}`);
+});
